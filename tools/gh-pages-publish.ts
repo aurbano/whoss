9a31a9e731ef13f2a1e42da4338b26a6d9de461a
Alejandro U. Alvarez <aluralma@gmail.com>
2018-04-03 17:23:54 +0100

    remove docs commit user infor

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -15,17 +15,14 @@ if (typeof pkg.repository === "object") {
 
 let parsedUrl = url.parse(repoUrl)
 let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
-let ghToken = process.env.GH_TOKEN
 
 echo("Deploying docs!!!")
 cd("docs")
 touch(".nojekyll")
 exec("git init")
 exec("git add .")
-exec('git config user.name "Alejandro U. Alvarez"')
-exec('git config user.email "aluralma@gmail.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
-  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
+  `git push --force --quiet "https://${repository}" master:gh-pages`
 )
 echo("Docs deployed!!")
